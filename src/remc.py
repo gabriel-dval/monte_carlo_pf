6709a1950f8adbd5c74e1fedab9c376a10b50261
gabriel-dval <gduvalkieffer@gmail.com>
2024-09-12 12:24:57 +0200

    updated remc function + rm pycache

--- src/remc.py
+++ src/remc.py
@@ -7,6 +7,7 @@ import numpy as np
 import math as m
 import os
 import sys
+import argparse
 
 import copy
 import random
@@ -17,7 +18,7 @@ import matplotlib.pyplot as plt
 
 from components import *
 
-# Main program function
+# Main program functions
 
 def remc_full_sim(sequence,
                   protein_name, 
@@ -47,6 +48,19 @@ def remc_full_sim(sequence,
     ---
     calculated_conformation
     '''
+    # Start messages
+
+    print(f"Running REMC simulation with the following parameters:")
+    print(f"Sequence: {sequence}")
+    print(f"Protein Name: {protein_name}")
+    print(f"Optimal Energy: {optimal_E}")
+    print(f"Min Temperature: {min_t}")
+    print(f"Max Temperature: {max_t}")
+    print(f"Monte Carlo Steps: {mc_steps}")
+    print(f"Number of Replicas: {replica_number}")
+    print(f"Cutoff Runtime: {cutoff_runtime} seconds")
+    print(f"Number of Runs: {nb_of_runs}")
+
     # Number elements of the input sequence
     n_sequence = [f'{aa.upper()}{i + 1}' for i, aa in enumerate(sequence)]
 
@@ -74,6 +88,39 @@ def remc_full_sim(sequence,
         plot_final_conformation(c, '../results', f'Run{i}')
 
 
+def main():
+    '''Argument parser for the main program function'''
+    parser = argparse.ArgumentParser(description="Run REMC simulation.")
+
+    # Required arguments
+    parser.add_argument("sequence", type=str, help="The protein sequence in HP format.")
+    parser.add_argument("protein_name", type=str, help="The name of the protein.")
+    parser.add_argument("optimal_E", type=float, help="The optimal energy level - if unknown a value must still be supplied.")
+
+    # Optional arguments with defaults
+    parser.add_argument("--min_t", type=int, default=160, help="Minimum temperature (default: 160).")
+    parser.add_argument("--max_t", type=int, default=220, help="Maximum temperature (default: 220).")
+    parser.add_argument("--mc_steps", type=int, default=500, help="Monte Carlo steps (default: 500).")
+    parser.add_argument("--replica_number", type=int, default=25, help="Number of replicas (default: 25).")
+    parser.add_argument("--cutoff_runtime", type=int, default=120, help="Cutoff runtime in seconds (default: 120).")
+    parser.add_argument("--nb_of_runs", type=int, default=5, help="Number of runs (default: 5).")
+
+    # Parse the arguments from the command line
+    args = parser.parse_args()
+
+    # Call the remc_full_sim function with the parsed arguments
+    remc_full_sim(
+        sequence=args.sequence,
+        protein_name=args.protein_name,
+        optimal_E=args.optimal_E,
+        min_t=args.min_t,
+        max_t=args.max_t,
+        mc_steps=args.mc_steps,
+        replica_number=args.replica_number,
+        cutoff_runtime=args.cutoff_runtime,
+        nb_of_runs=args.nb_of_runs
+    )
+
 
 if __name__ == "__main__":
-    print('gsjdfl')
+    main()
